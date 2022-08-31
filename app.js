28b89a3f210b188e65379a23f4e48ed4c9c58671
Agustin Costa Ditarcio <agustin.costa.995@gmail.com>
2022-08-30 21:30:37 -0300

    Empezando el Home

---
+++ app.js
@@ -0,0 +1,20 @@
+let express = require("express");
+let path = require("path")
+
+let app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(3000, () => console.log("Servidor corriendo en el puerto 3000"));
+
+app.get("/", function(req,res) {
+    res.sendFile(path.join(__dirname, "/views/index.html"))
+});
+
+app.get("/register", function(req,res) {
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+})
+
+app.get("/login", function(req,res) {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+})
