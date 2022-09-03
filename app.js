a5728de73ef0f7dcb86be088e7073ff5d56fafe9
JulianGallegos02 <julianezequielgallegos@gmail.com>
2022-09-03 17:21:17 -0300

    home/productDetail terminados, revisar estilos

--- app.js
+++ app.js
@@ -5,20 +5,20 @@ let app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(3000, () => console.log("Servidor corriendo en http://localhost:3000"));
+app.listen(3000, () => console.log("Servidor corriendo en el puerto 3000"));
 
 app.get("/", function(req,res) {
     res.sendFile(path.join(__dirname, "/views/index.html"))
 });
 
+app.get("/productDetail", function(req,res) {
+    res.sendFile(path.join(__dirname, "/views/productDetail.html"))
+})
+
 app.get("/register", function(req,res) {
     res.sendFile(path.join(__dirname, "/views/register.html"))
-});
+})
 
-app.get("/login", function(req,res) {         
-    res.sendFile(path.join(__dirname, "/views/login.html")) 
-});
-
-app.get("/productDetail", function(req,res) {         
-    res.sendFile(path.join(__dirname, "/views/productDetail.html")) 
-});
+app.get("/login", function(req,res) {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+})
