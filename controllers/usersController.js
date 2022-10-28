930b432801b45979fd6ecc7e2c016fb7a58873b3
Agustin Costa Ditarcio <agustin.costa.995@gmail.com>
2022-10-27 23:12:18 -0300

    Sprint5: Registro de usuarios funcional

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+
+function findAll() {
+    const jsonData = fs.readFileSync(path.join(__dirname, "../data/users.json"));
+    const data = JSON.parse(jsonData);
+    return data
+};
+
+function create(data) {
+    const dataString = JSON.stringify(data, null, 4);
+    fs.writeFileSync(path.join(__dirname, "../data/users.json"), dataString)
+};
+
 const controller = {
 
     login: function(req, res){
@@ -7,7 +21,25 @@ const controller = {
     register: function(req, res){
         res.render("register", {style: "register"});
     },
+    addUser: function(req, res){
+        const data = findAll()
 
+        const newUser = {
+            id: data.length + 1,
+            nombre: req.body.nombre,
+            apellido: req.body.apellido,
+            email: req.body.email,
+            contraseña: req.body.password,
+            imagen: req.file.filename
+
+        }
+
+        data.push(newUser);
+
+        create(data);
+
+        res.redirect("/");
+    }
 }
 
 module.exports = controller;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 const usersController = require("../controllers/usersController");
+const multer = require('multer');
+const path = require('path')
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, path.join(__dirname, '../public/images/users'));
+    },
+    filename: function(req, file, cb){
+        const newFileName = file.fieldname + Date.now() + "-" + path.extname(file.originalname);
+        cb(null, newFileName )
+    }
+})
+
+const upload = multer({storage: storage})
 
 /* GET users listing. */
 router.get('/login', usersController.login);
 router.get('/register', usersController.register);
+router.post('/register', upload.single('avatar'), usersController.addUser)
 
 module.exports = router;
