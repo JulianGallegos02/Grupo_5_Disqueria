f2ac0aeabbc2b5432bd93b47e79e5c6e694e1b2d
JulianGallegos02 <julianezequielgallegos@gmail.com>
2022-10-30 05:16:29 -0300

    revisar home para dividir los productos

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,6 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+
+function findAll() {
+    const jsonData = fs.readFileSync(path.join(__dirname, "../data/products.json"));
+    const data = JSON.parse(jsonData);
+    return data
+};
+
+function create(data) {
+    const dataString = JSON.stringify(data, null, 4);
+    fs.writeFileSync(path.join(__dirname, "../data/products.json"), dataString)
+};
+
+
 const controller = {
-    home: function(req, res) {       
-        res.render("index", {style: "styles"});
+  
+    home: function(req, res) {     
+        const data = findAll();
+      
+      
+        res.render("index", {style: "styles", products: data });
       },
 
 }
